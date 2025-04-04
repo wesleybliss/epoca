6c4d1731f64ea5c717dbcb551711a95eed787b7d
Wesley Bliss <wesley.bliss@gmail.com>
2025-04-04 00:05:12 -0500

    Added Vercel preset for deployment

--- app.config.ts
+++ app.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
     tsr: {
         appDirectory: 'src',
     },
+    server: {
+        preset: 'vercel',
+    },
     vite: {
         plugins: [
             tsConfigPaths({
